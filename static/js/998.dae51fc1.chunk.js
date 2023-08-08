0a244871539188c014708121a0e6a877c8106bd1
Tasmyt <121162710+Tasmyt@users.noreply.github.com>
2023-08-08 12:55:32 +0000

    Deploying to gh-pages from @ Tasmyt/goit-react-hw-08-phonebook@20fb21ce30ce4150bca14459f9fa7489eeb1770e 🚀

---
+++ static/js/998.dae51fc1.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[998],{998:function(e,o,t){t.r(o),t.d(o,{default:function(){return r}});var n=t(184);function r(){return(0,n.jsxs)("section",{children:[(0,n.jsx)("title",{children:"Wellcome to you PhoneBook"}),(0,n.jsx)("h2",{children:"Please register or login to your account to use PhoneBook"})]})}}}]);
+//# sourceMappingURL=998.dae51fc1.chunk.js.map
